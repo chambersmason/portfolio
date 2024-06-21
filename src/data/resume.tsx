1d2c0539008492ebb7eb8d1d7b9056483dbc5187
Mason Chambers <masonchambers@Mason-M3.local>
2024-06-20 18:05:02 -0700

    portfolio init

---
+++ src/data/resume.tsx
@@ -0,0 +1,128 @@
+import { Icons } from "@/components/icons";
+
+export const DATA = {
+    "name": "Mason Chambers",
+    "initials": "MC",
+    "url": "https://masonchambers.com",
+    "location": "Los Angeles, CA",
+    "locationLink": "https://www.google.com/maps/place/losangeles",
+    "description": "Software Engineer turned Entrepreneur. On the constant pursuit of knowledge.",
+    "summary": "Currently building AI solutions for Industry Inteligence. Building Scripter on the side. Pusuing a Masters in Artificial Intelligence and Computer Science in 2025.",
+    "avatarUrl": "/me.png",
+    "skills": [
+      "Java",
+      "JavaScript",
+      "Python",
+      "TypeScript",
+      "SQL",
+      "C++",
+      "Swift",
+      "CSS",
+      "XML",
+      "React",
+      "Unix",
+      "Git",
+      "MySQL",
+      "MongoDB",
+      "Postgres",
+      "SAP",
+      "Docker",
+      "Xcode",
+      "AWS",
+      "Vector Database",
+      "Hugging Face",
+      "Lambda",
+      "EC2",
+      "RDS",
+      "ChakraUI",
+    ],
+    "contact": {
+      "email": "masonchambers.compsci@gmail.com",
+      "tel": "+4439883064",
+      "social": {
+        "GitHub": {
+          "url": "https://github.com/dillionverma",
+          "icon": "Icons.github"
+        },
+        "LinkedIn": {
+          "url": "https://www.linkedin.com/in/mason-chambers/",
+          "icon": "Icons.linkedin"
+        },
+        "X": {
+          "url": "https://x.com/mason_compsci",
+          "icon": "Icons.x"
+        }
+      }
+    },
+    "work": [
+      {
+        "company": "Industry Intelligence Inc",
+        "href": "",
+        "badges": [],
+        "location": "Los Angeles, CA",
+        "title": "AI Software Solutions Engineer",
+        "logoUrl": "/iii1.jpeg",
+        "start": "June 2023",
+        "end": "Present",
+        "description": "Spearheaded AI operations and deployment initiatives across IT, Sales, and Content departments. Designed and deployed a legislative analysis tool that aggregates and custom-generates reports for businesses based on relevant federal and state legislation. This tool processes the potential impact of bills and tags the data with proprietary data – significantly enhancing decision-making for impacted industries. Over 500k pieces of legislation processed ytd. Increased monthly content generation by 900%. Developed a program that processes over 1.5k companies’ social media posts daily, compiling them into AI-generated news articles. These articles provide insights into their social media strategies and the industry relevance of their content, resulting in over 30k articles. Engineered an AI-driven admin tool for internal use, enabling automatic enrichment of legacy article tagging processes by translating natural language to our proprietary tagging schema. Developed a solution integrating multiple large language model APIs with Langchain and vector databases, utilizing a catalog of over 10 million articles to fine-tune a bot for answering industry-specific questions. Developed a cutting-edge system that utilizes a Retrieval-Augmented Generation (RAG) and agentic workflows for multi-label classification on social media, news, patents, stock reports, finances, legislation. Upgraded article search capabilities by incorporating vector search, transforming searches into a bespoke tagging language for Industry Intelligence, improving search accuracy and speed for client news data. Orchestrated annual project timelines covering both internal tools and market releases, including setting deadlines, budgeting, establishing quality assurance benchmarks, and strategic planning for future endeavors. Collaborated closely with the sales team and CEO to optimize monetization strategies for new projects. Implemented an AI module for detecting duplicate articles from different sources and authors."
+      },
+      {
+        "company": "Prometheus Group",
+        "href": "",
+        "badges": [],
+        "location": "Raleigh, NC",
+        "title": "Senior Full-Stack Web Developer",
+        "logoUrl": "/promo.webp",
+        "start": "June 2020",
+        "end": "January 2023",
+        "description": "Developed and optimized custom version of the Vulcan web app, an ERP and asset management solution, for 20+ major clients including Google, Chevron, and Coca-Cola. Contributed 5000+ commits to Vulcan and its associated microservices utilizing React, Redux, TypeScript, JavaScript, Java, C++, SQL, Docker, AWS, SAP, and ABAP. Engaged directly with customers, project managers, and implementation teams to identify specific requirements and design tailored solutions; utilized Jira, Confluence, and Git to monitor and manage project progress effectively. Participated in customer debugging sessions, offering comprehensive support from initial diagnosis to final deployment, ensuring seamless integration and satisfaction. Conducted thorough code reviews, facilitated bug resolution, and allocated tasks among team members to optimize workflow efficiency and product quality. Mentored and trained new developers, sharing best practices to strengthen the team's collective expertise. Spearheaded scrum meetings, diligently tracked release schedules, and efficiently managed deadlines across multiple concurrent projects, ensuring timely delivery and client satisfaction."
+      },
+      {
+        "company": "Avid",
+        "href": "",
+        "badges": [],
+        "location": "Burlington, MA",
+        "title": "Full-Stack Software Engineer Intern",
+        "logoUrl": "/avid.svg",
+        "start": "May 2019",
+        "end": "September 2019",
+        "description": "Developed the initial microservice integrating Frame.io’s RESTful API into Media Composer’s web application for streamlined cloud-based video sharing. Designed a full-stack Python application for daily regression and stress testing of the Media Composer Editorial Management API. Collaborated directly with the lead software architect for MCEM on bug resolution, mentorship, and design."
+      }
+    ],
+    "education": [
+      {
+        "school": "Elon University",
+        "href": "https://www.elon.edu/u/news/2019/04/10/computer-science-team-wins-triad-programming-competition/",
+        "degree": "B.S. in Computer Science & Minor in Communications",
+        "logoUrl": "/kisspng-elon-university-johnson-c-smith-university-east-c-phoenix-5ace5ecd9d42c6.3950749015234741256441.png",
+        "start": "2016",
+        "end": "2020",
+      }
+    ],
+    "projects": [
+      {
+        "title": "Scripter Studios",
+        "href": "https://scripter-v1.com",
+        "dates": "Jan 2024 - Feb 2024",
+        "active": true,
+        "description": "Founders University Cohort 8 Incubator. Using Ai to empower Hollywood executives.",
+        "technologies": [
+          "Next.js",
+          "Machine Learning",
+          "LLM",
+          "Vectorspaces",
+          "TailwindCSS",
+          "Stripe",
+        ],
+        "links": [
+          {
+            "type": "Website",
+            "href": "https://scripter-v1.com",
+            // "icon": "<Icons.globe className='size-3' />"
+          }
+        ],
+        // "image": "scripterthing.png",
+        // "video": "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4"
+      }
+    ]
+  }
